b5a7b7724a72f00171669c257525d3e5e1af07f6
xd6d <kurkin566@gmail.com>
2023-11-20 16:57:04 +0200

    deleted unused class

--- src/com/solvd/laba/block1/oop/exceptions/BucketOverflowException.java
+++
@@ -1,7 +0,0 @@
-package com.solvd.laba.block1.oop.exceptions;
-
-public class BucketOverflowException extends Exception {
-    public BucketOverflowException(String message) {
-        super(message);
-    }
-}
