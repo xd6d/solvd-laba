6ad1558e2f66ea40eb46cff070afefa7093371cb
xd6d <kurkin566@gmail.com>
2023-11-25 20:46:16 +0200

    code appearance

--- src/main/java/com/solvd/laba/block1/apachelib/Demo.java
+++ src/main/java/com/solvd/laba/block1/apachelib/Demo.java
@@ -6,7 +6,8 @@ public class Demo {
     public static void main(String[] args) {
         WordService wordService = new WordService();
         wordService.writeToFileSortedByNumber(
-                wordService.getUnique(new File("input.txt")), new File("output.txt")
+                wordService.getUnique(new File("input.txt")),
+                new File("output.txt")
         );
     }
 }
