99780bf727a687c66be1c4649b141bb5f316b7ba
xd6d <kurkin566@gmail.com>
2023-11-27 16:17:14 +0200

    fixed logic

--- src/main/java/com/solvd/laba/block1/apachelib/Demo.java
+++ src/main/java/com/solvd/laba/block1/apachelib/Demo.java
@@ -1,13 +1,15 @@
 package com.solvd.laba.block1.apachelib;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.File;
 
 public class Demo {
+    private static final Logger LOGGER = LogManager.getLogger(Demo.class);
+
     public static void main(String[] args) {
         WordService wordService = new WordService();
-        wordService.writeToFileSortedByNumber(
-                wordService.getUnique(new File("input.txt")),
-                new File("output.txt")
-        );
+        LOGGER.info("Number of unique words: " + wordService.getUnique(new File("input.txt")));
     }
 }
--- src/main/java/com/solvd/laba/block1/apachelib/WordService.java
+++ src/main/java/com/solvd/laba/block1/apachelib/WordService.java
@@ -8,43 +8,21 @@ import org.apache.logging.log4j.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class WordService {
     private static final Logger LOGGER = LogManager.getLogger(WordService.class);
 
     private static final String EXCEPTION_MESSAGE = "Resolved %s";
 
-    public Map<String, Integer> getUnique(File file) {
-        Map<String, Integer> words = new HashMap<>();
+    public int getUnique(File file) {
         try {
             String text = FileUtils.readFileToString(file, Charset.defaultCharset());
-            for (String word : StringUtils.split(text, " \t\n\r[]().,/|\\`\"!@#№;:$%^?")) {
-                word = StringUtils.toRootLowerCase(word);
-                if (words.containsKey(word)) {
-                    words.put(word, words.get(word) + 1);
-                } else {
-                    words.put(word, 1);
-                }
-            }
+            return Arrays.stream(StringUtils.split(text, " \t\n\r[]().,/|\\`\"!@#№;:$%^?")).collect(Collectors.toSet()).size();
         } catch (IOException e) {
             LOGGER.warn(EXCEPTION_MESSAGE.formatted(e));
         }
-        return words;
-    }
-
-    public void writeToFileSortedByNumber(Map<String, Integer> words, File file) {
-        List<String> lines = words.entrySet()
-                .stream()
-                .sorted((e1, e2) -> e2.getValue() - e1.getValue())
-                .map(e -> e.getKey() + ": " + e.getValue())
-                .toList();
-        try {
-            FileUtils.writeLines(file, lines);
-        } catch (IOException e) {
-            LOGGER.warn(EXCEPTION_MESSAGE.formatted(e));
-        }
+        return 0;
     }
 }
