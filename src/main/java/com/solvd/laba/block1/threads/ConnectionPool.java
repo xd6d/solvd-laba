e9e1e87cde545eab33f2a6a30be9e9f0eb772de1
xd6d <kurkin566@gmail.com>
2023-12-11 16:44:27 +0200

    filled Connection class

--- src/main/java/com/solvd/laba/block1/threads/ConnectionPool.java
+++ src/main/java/com/solvd/laba/block1/threads/ConnectionPool.java
@@ -13,7 +13,7 @@ public class ConnectionPool {
 
     private ConnectionPool(int size) {
         for (int i = 0; i < size; i++) {
-            connections.add(new Connection());
+            connections.add(new Connection(i));
         }
     }
 
